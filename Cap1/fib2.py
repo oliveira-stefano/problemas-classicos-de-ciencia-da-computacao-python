d9920081757d4f08cc68ba57da72bed580424461
Stefano Oliveira <oliveira.stefano@gmail.com>
2020-03-05 10:47:24 -0300

    Cap2

---
+++ Cap1/fib2.py
@@ -0,0 +1,32 @@
+''' 
+
+Evitar uma recursao infinita é responsabilidade do programador, e não do compilador pu do interpretador.
+No exemplo anterior o erro ocorria porque nào especificamos um caso de base. Em uma funcao recursiva
+um caso de base serve como ponto de partida
+
+'''
+def fib2(n: int) -> int:
+    '''
+    Exemplo de saida para fib2(4)
+    fib2(4) -> fib2(3), fib2(2) = 3
+    fib2(3) -> fib2(2), fib2(1) = 2
+    fib2(2) -> fib2(1), fib2(0) = 1
+    fib2(2) -> fib2(1), fib2(0) = 1
+    fib2(1) ->                  = 1
+    fib2(1) ->                  = 1
+    fib2(1) ->                  = 1
+    fib2(0) ->                  = 0
+    fib2(0) ->                  = 0
+    '''
+    if n < 2:
+        return n #caso de base
+    else:
+        return fib2(n - 2) + fib2(n - 1)
+
+if __name__ == "__main__":
+    print(fib2(4))
+
+
+
+
+    
